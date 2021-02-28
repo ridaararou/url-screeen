36b30b697d891bcccbdc86916fc74aac564f3fa9
rida <rida@kreatinc.com>
2021-02-28 15:03:37 +0100

    added error text

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/:type?', (req, res) => {
     try {
 
       //url: 'https://l2l-develop.boringserver.com/ssi-editor-generate-template.html?templateType=instagram&templateData=%7B%20%22template%22%3A%204%2C%20%22colors%22%3A%20%7B%22primary%22%3A%20%22rgba(170%2C%20200%2C%2040%2C%200.9)%22%2C%20%22secondary%22%3A%20%22%23f09%22%7D%2C%20%22font%22%3A%20%22Avenir%20Next%22%2C%20%22title%22%3A%20%22OPEN%20HOUSE%20in%20Martil%22%2C%20%22subtitle%22%3A%20%22Click%20Or%20Text%209494%20to%209999%20for%20More%20Photos!%22%2C%20%22textPosition%22%3A%20%22bottom%22%2C%20%22moreImgNb%22%3A%20%2220%22%2C%20%22agent%22%20%3A%20%7B%20%22src%22%3A%20%22https%3A%2F%2Fimages.pexels.com%2Fphotos%2F220453%2Fpexels-photo-220453.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D300%22%2C%20%22position%22%3A%20%22left%20top%22%2C%20%22rotation%22%3A%20%220%22%20%7D%2C%20%22images%22%3A%20%5B%20%7B%20%22src%22%3A%20%22tools%2Fsocial-share-image-templates%2Fimg%2F1.jpg%22%2C%20%22position%22%3A%20%22center%22%2C%20%22rotation%22%3A%20%220%22%20%7D%2C%20%7B%20%22src%22%3A%20%22tools%2Fsocial-share-image-templates%2Fimg%2F2.jpg%22%2C%20%22position%22%3A%20%22right%20bottom%22%2C%20%22rotation%22%3A%20%220%22%20%7D%2C%20%7B%20%22src%22%3A%20%22tools%2Fsocial-share-image-templates%2Fimg%2F3.jpg%22%2C%20%22position%22%3A%20%22center%22%2C%20%22rotation%22%3A%20%220%22%20%7D%2C%20%7B%20%22src%22%3A%20%22tools%2Fsocial-share-image-templates%2Fimg%2F4.jpg%22%2C%20%22position%22%3A%20%22left%20top%22%2C%20%22rotation%22%3A%20%220%22%20%7D%20%5D%20%7D';
-      var siteUrl = (req.query.url) ? (req.query.url) : 'https://l2l-develop.boringserver.com/ssi-editor-generate-template.html?templateType=facebook&templateData';
+      var siteUrl = (req.query.url) ? (req.query.url) : 'https://l2l-develop.boringserver.com/ssi-editor-generate-template.html?templateType=instagram&templateData';
       var width = (req.query.width) ? (req.query.width) : 1200;
       var height = (req.query.height) ? (req.query.height) : 1200;
 
@@ -53,7 +53,7 @@ app.get('/:type?', (req, res) => {
 
     } catch (error) {
       console.error('oops, something went wrong!' + error);
-      res.json({error: 'something went worng!'});
+      res.json({error: 'something went worng!' + error});
       // res.send('<h1>Error!! '+ error);
     }
   })();
